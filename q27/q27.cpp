8ec35bb480eac13e9d4a1c40229aa9c87199cb88
lalalalife <hongoops@gmail.com>
2023-03-11 16:22:43 +0900

    이코테 이진탐색

---
+++ q27/q27.cpp
@@ -0,0 +1,33 @@
+﻿// q27.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int n, x; 
+vector<int>v; 
+
+int binary_search()
+{
+    vector<int>::iterator itStart = lower_bound(v.begin(), v.end(), x); 
+    vector<int>::iterator itEnd = upper_bound(v.begin(), v.end(), x); 
+    return itEnd - itStart; 
+}
+
+int main()
+{
+    cin >> n >> x; 
+    for (int i = 0; i < n; i++) {
+        int m;
+        cin >> m; 
+        v.push_back(m); 
+    }
+    
+    int count = binary_search(); 
+    if (count == 0)
+        cout << "-1" << endl;
+    else
+        cout << count << endl; 
+}
+
