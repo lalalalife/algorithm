8c6b5a5630dead90489fe00a5d3c9a0e25ee04fa
lalalalife <hongoops@gmail.com>
2023-03-13 06:52:59 +0900

    이코테 dp

---
+++ q33/q33.cpp
@@ -0,0 +1,38 @@
+﻿// q33.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int n; 
+vector<int> t; 
+vector<int> p; 
+int dp[16]; 
+int maxValue; 
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        int x, y; 
+        cin >> x >> y; 
+        t.push_back(x); 
+        p.push_back(y); 
+    }
+
+    // 리스트 뒤에서부터 확인
+    for (int i = n - 1; i >= 0; i--) {
+        int time = t[i] + i;   // ✔ (상담이 종료되는 날짜)
+        if (time <= n) {
+            dp[i] = max(p[i] + dp[time], maxValue);  // ✔ 점화식
+            maxValue = dp[i]; 
+        }
+        else {
+            dp[i] = maxValue; 
+        }
+    }
+
+    cout << maxValue << '\n'; 
+
+}
