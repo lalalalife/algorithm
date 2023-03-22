a0d91f9a9782991cb7dd84fbeb37ce5674e84fd1
lalalalife <hongoops@gmail.com>
2023-03-22 20:43:20 +0900

    이코테 위상정렬

---
+++ q45/q45.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std; 
+
+int testCase, n, m; 
+
+// 진입차수 
+int indegree[501]; 
+
+// 노드에 연결된 간선정보 (a가 연결되어 있는 b../??
+bool graph[501][501]; 
+
+int main()
+{
+    cin >> testCase; 
+
+    for (int tc = 0; tc < testCase; tc++) {
+        fill(indegree, indegree + 501, 0);      // 진입차수 배열 0으로 초기화 
+        for (int i = 0; i < 501; i++) {
+            fill(graph[i], graph[i] + 501, false);      // 
+        }
+
+        cin >> n;       // 팀 갯수
+        vector<int> v; 
+        for (int i = 0; i < n; i++) {
+            int x; 
+            cin >> x; 
+            v.push_back(x);         // 작년 순위 
+        }
+
+        // 간선정보 초기화 ? 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                graph[v[i]][v[j]] = true;       // ...
+                indegree[v[j]] += 1; 
+            }
+        }
+
+        // 변경된 순위 정보 반영 ✔
+        cin >> m; 
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            cin >> a >> b; 
+            if (graph[a][b]) {
+                graph[a][b] = false; 
+                graph[b][a] = true; 
+                indegree[a] += 1; 
+                indegree[b] -= 1; 
+            }
+            else {
+                graph[a][b] = true;
+                graph[b][a] = false;
+                indegree[a] -= 1;
+                indegree[b] += 1;
+            }
+        }
+
+        // 위상정렬
+        vector<int> result; 
+        queue<int> q; 
+
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i); 
+            }
+        }
+
+        bool certain = true; 
+        bool cycle = false; 
+
+        for (int i = 0; i < n; i++) {
+            if (q.size() == 0) {
+                cycle = true; 
+                break; 
+            }
+
+            if (q.size() >= 2) {
+                certain = false; 
+                break; 
+            }
+
+            int now = q.front(); 
+            q.pop(); 
+            result.push_back(now); 
+
+            // 연결된 노드 찾아서 진입차수 -1 
+            for (int j = 1; j <= n; j++) {
+                if (graph[now][j]) {
+                    indegree[j] -= 1; 
+                    if (indegree[j] == 0) {
+                        q.push(j); 
+                    }
+                }
+            }
+        }
+
+        // 결과
+        if (cycle)
+            cout << "IMPOSSIBLE" << endl;
+        else if (!certain)
+            cout << "?" << endl; 
+        else {
+            for (int i = 0; i < result.size(); i++) {
+                cout << result[i] << ' '; 
+            }
+            cout << endl; 
+        }
+    }
+}
