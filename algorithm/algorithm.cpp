dc75d9112baa6ae5e45e3d4f080796bd3a13ef33
lalalalife <hongoops@gmail.com>
2023-03-31 07:08:49 +0900

    이코테 dfs,bfs

---
+++ algorithm/algorithm.cpp
@@ -0,0 +1,168 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void exam4_1()
+{
+	int n; 
+	string plan; 
+	// L, R, U, D 
+	int dx[] = { 0, 0, -1, 1 };
+	int dy[] = { -1, 1, 0, 0 };
+	char moves[] = { 'L', 'R', 'U', 'D' }; 
+
+	cin >> n;
+	cin.ignore(); 
+	getline(cin, plan); 
+
+
+	int x = 1, y = 1; 
+	for (int i = 0; i < plan.size(); i++) {
+		
+		int nx = -1; 
+		int ny = -1; 
+
+		for (int j = 0; j < 4; j++) {
+			if (plan[i] == moves[j]) {
+				nx = x + dx[j];
+				ny = y + dy[j];
+				break; 
+			}
+		}		
+
+		if (nx >= 1 && ny >= 1 && nx <= n && ny <= n) {
+			x = nx; 
+			y = ny; 
+		}
+	}
+
+	cout << x << ' ' << y << endl; 
+}
+
+void exam4_2()
+{
+	int n; 
+	cin >> n; 
+
+	// 00:00:00 ~ N:59:59
+	int result = 0; 
+
+	for (int i = 0; i <= n; i++){
+		for (int j = 0; j <= 59; j++) {
+			for (int k = 0; k <= 59; k++) {
+				if (i / 10 == 3 || i % 10 == 3 || j / 10 == 3 || j % 10 == 3 || k / 10 == 3 || k % 10 == 3) {
+					result++; 
+				}
+			}
+		}
+	}
+
+	cout << result << endl; 
+}
+
+void exam4_3()
+{	
+	//a1 
+	string n; 
+	cin >> n; 
+
+	int x = n[0] - 'a' + 1; 
+	int y = n[1] - '0';
+
+	int step[8][2] = { {1, 2}, {-1, 2}, {1, -2}, {-1, -2}, {2, 1}, {2, -1}, {-2, 1}, {-2, -1} }; 
+
+	int result = 0; 
+	for (int i = 0; i < 8; i++) {
+		int nx = x + step[i][0]; 
+		int ny = y + step[i][1]; 
+
+		if (nx > 0 && ny > 0 && nx <= 8 && ny <= 8) {
+			result++; 
+		}
+	}
+	cout << result << endl; 
+}
+
+int n, m;
+int x, y, d;
+
+void turn_left()
+{
+	d--; 
+	if (d < 0)
+		d = 3; 		
+}
+
+void exam4_4()
+{
+	
+	int arr[50][50]; 
+
+	cin >> n >> m; 	
+	cin >> x >> y >> d; 
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j]; 
+		}
+	}
+
+	// 북서남동 이동좌표
+	int dx[] = { -1, 0, 1, 0 }; 
+	int dy[] = { 0, -1, 0, 1 }; 
+
+	arr[x][y] = 2; // 처음위치한곳 1로 바꾸고 시작 
+
+	int cnt = 0; 
+	int turn_time = 0; 
+
+	while (true) {
+
+		turn_left(); 
+		int nx = x + dx[d]; 
+		int ny = y + dy[d]; 
+
+		if (arr[nx][ny] == 0) {
+			arr[nx][ny] = 2;	// 방문표시
+			x = nx; 
+			y = ny; 
+			cnt++; 
+
+			turn_time = 0; 
+			continue; 
+		}
+		else {
+			turn_time++; 
+		}
+
+		if (turn_time == 4) {
+			// 한칸뒤로 
+			nx = x - dx[d]; 
+			ny = y - dy[d]; 
+
+			if (arr[nx][ny] == 1) {
+				break; 
+			}
+			else {
+				x = nx; 
+				y = ny; 
+				cnt++;
+			}
+
+			turn_time = 0; 
+		}
+	}
+		
+	cout << cnt << endl; 
+}
+
+int main()
+{
+	//exam4_1(); 
+	//exam4_2();
+
+	//exam4_3(); 
+	exam4_4(); 
+}
---
+++ dfsbfs/dfsbfs.cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+using namespace std;
+
+void recursive(int i)
+{
+    if (i == 10)
+        return;
+
+    cout << i << "번째 호출 " << endl; 
+    recursive(i+1);
+
+    cout << i << "번째 호출 종료" << endl;
+}
+
+bool visited[9];
+vector<int> graph[9];
+
+void dfs_exam1_dfs(int x)
+{    
+    visited[x] = true; // 노드 방문처리
+ 
+    cout << x << ' ';       // 노드 방문 순서 
+
+    // 현재노드와 연결된 다른 노드 방문
+    for (int i = 0; i < graph[x].size(); i++) {
+        int y = graph[x][i]; 
+        if (!visited[y])
+            dfs_exam1_dfs(y); 
+    }
+}
+
+void dfs_exam1()
+{    
+    // 그럼 이건 인접행렬?
+    graph[1].push_back(2); 
+    graph[1].push_back(3);
+    graph[1].push_back(8);
+
+    graph[2].push_back(1);
+    graph[2].push_back(7);
+
+    graph[3].push_back(1);
+    graph[3].push_back(4);
+    graph[3].push_back(5);
+
+    graph[4].push_back(3);
+    graph[4].push_back(5);
+
+    graph[5].push_back(3);
+    graph[5].push_back(4);
+
+    graph[6].push_back(7);
+
+    graph[7].push_back(2);
+    graph[7].push_back(6);
+    graph[7].push_back(8);
+
+    graph[8].push_back(1);
+    graph[8].push_back(7);
+
+    dfs_exam1_dfs(1); 
+}
+
+
+
+void bfs_exam1_bfs(int start)
+{
+    // bfs 큐에 넣고 방문처리 
+
+    queue<int> q; 
+    q.push(start); 
+    visited[start] = true;
+
+    while (!q.empty()) {
+        int x = q.front(); 
+        q.pop(); 
+    
+        cout << x << ' '; 
+
+        for (int i = 0; i < graph[x].size(); i++) {
+            int y = graph[x][i]; 
+            if (!visited[y]) {
+                q.push(y);
+                visited[y] = true;
+            }
+        }
+    }
+}
+
+void bfs_exam1()
+{
+    graph[1].push_back(2);
+    graph[1].push_back(3);
+    graph[1].push_back(8);
+
+    graph[2].push_back(1);
+    graph[2].push_back(7);
+
+    graph[3].push_back(1);
+    graph[3].push_back(4);
+    graph[3].push_back(5);
+
+    graph[4].push_back(3);
+    graph[4].push_back(5);
+
+    graph[5].push_back(3);
+    graph[5].push_back(4);
+
+    graph[6].push_back(7);
+
+    graph[7].push_back(2);
+    graph[7].push_back(6);
+    graph[7].push_back(8);
+
+    graph[8].push_back(1);
+    graph[8].push_back(7);
+
+    bfs_exam1_bfs(1);
+}
+
+int main()
+{
+    //recursive(1); 
+    //dfs_exam1(); 
+
+    bfs_exam1(); 
+}
