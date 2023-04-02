4a34d1eda12e529801f4def97cde8022c156971a
lalalalife <hongoops@gmail.com>
2023-04-03 06:59:32 +0900

    이코테 정렬

---
+++ sort_2/sort_2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+bool compare(int x, int y)
+{
+	return x > y; 
+}
+
+int main()
+{
+	int n; 
+	cin >> n; 
+
+	vector<int> v(n); 
+
+	for (int i = 0; i < n; i++) {
+		int m; 
+		cin >> m; 
+		v.push_back(m); 
+	}
+	
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i] << ' '; 
+	}
+}
