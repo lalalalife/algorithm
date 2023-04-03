586aa70aff19f54f0212f86d116e9220986e6d18
lalalalife <hongoops@gmail.com>
2023-04-03 20:51:43 +0900

    이코테 정렬

---
+++ sort_4/sort_4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool compare(int a, int b) {
+    return a > b; 
+}
+
+int main()
+{
+    int n, k; 
+    cin >> n >> k; 
+    vector<int> a(n); 
+    vector<int> b(n);
+
+    for (int i = 0; i < n; i++) {
+        int m; 
+        cin >> m;
+        a[i] = m; 
+    }
+
+    for (int i = 0; i < n; i++) {
+        int m;
+        cin >> m;
+        b[i] = m;
+    }
+
+    sort(a.begin(), a.end()); 
+    sort(b.begin(), b.end(), compare); 
+
+    for (int i = 0; i < k; i++) {
+        if (a[i] < b[i])
+            swap(a[i], b[i]);
+        else
+            break; 
+    }
+
+    int result = 0; 
+    for (int i = 0; i < n; i++) {
+        result += a[i]; 
+    }
+
+    cout << result << endl; 
+}
