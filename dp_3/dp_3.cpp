7f172c7d4fc71b97655fdd1ee7437cd7bfc829ed
lalalalife <hongoops@gmail.com>
2023-04-06 06:22:37 +0900

    이코테 다이나믹

---
+++ dp_3/dp_3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n; 
+    vector<int> k; 
+    int dp[100] = { 0, };        // n-1, n-2한 값의 결과가 누적 
+    cin >> n; 
+
+    for (int i = 0; i < n; i++) {
+        int m; 
+        cin >> m; 
+        k.push_back(m); 
+    }
+
+    dp[0] = k[0]; 
+    dp[1] = max(k[0], k[1]); 
+
+    for (int i = 2; i < n; i++) {
+        dp[i] = max(dp[i - 1], dp[i - 2] + k[i]); 
+    }
+
+    cout << dp[n - 1] << endl; 
+}
