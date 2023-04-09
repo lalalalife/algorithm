13ccb3c5893f8157b718dd88461ab3bdc206a76e
lalalalife <hongoops@gmail.com>
2023-04-10 07:03:37 +0900

    백준 플로이드워셜 경로탐색

---
+++ dijkstra_6/dijkstra_6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int graph[100][100];
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> graph[i][j];
+		}
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int a = 0; a < n; a++) {
+			for (int b = 0; b < n; b++) {
+
+				if (graph[a][k] == 1 && graph[k][b] == 1)
+					graph[a][b] = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << graph[i][j] << ' ';
+		}
+		cout << endl;
+	}
+}
