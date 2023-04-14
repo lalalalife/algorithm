184d975cd41fec0cf171968816f9832e0a654a70
lalalalife <hongoops@gmail.com>
2023-04-15 08:39:11 +0900

    곱하기혹은더하기

---
+++ q02/q02.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    string s; 
+    cin >> s; 
+
+    int result = s[0]-'0';
+
+    for (int i = 1; i < s.length(); i++) {
+        int n = s[i]-'0'; 
+
+        if (result >= 2 && n >= 2) {
+            result *= n; 
+        }
+        else {
+            result += n; 
+        }
+    }
+
+    cout << result << endl; 
+}
+
