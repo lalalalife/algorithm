3649c4d8a829d0cf2be7eb0b5701ba2e75adaa5f
lalalalife <hongoops@gmail.com>
2023-05-09 07:04:25 +0900

    백준dfs

---
+++ 0508_baekjoon2606/0508_baekjoon2606.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std; 
+
+int n, m; 
+vector<int> v[101];
+bool visited[101]; 
+int result = 0; 
+
+void dfs(int x)
+{
+    for (int i = 0; i < v[x].size(); i++) {
+        int next = v[x][i]; 
+        if (!visited[next]) {
+            visited[next] = true;
+            result++;
+            dfs(next); 
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m; 
+
+    for (int i = 0; i < m; i++) {
+        int a, b; 
+        cin >> a >> b; 
+        v[a].push_back(b); 
+        v[b].push_back(a);
+    }
+    
+    visited[1] = true; 
+    dfs(1); 
+
+    cout << result << endl; 
+}
