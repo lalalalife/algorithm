54e010a5643300933521856e1f4f43457e225d5d
lalalalife <hongoops@gmail.com>
2023-05-23 06:47:10 +0900

    백준 dfs

---
+++ bk_2468/bk_2468.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std; 
+
+int n; 
+int board[100][100]; 
+bool safezone[100][100];
+bool visited[100][100];
+
+void initSafeZoen()
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			safezone[i][j] = true; 			
+			visited[i][j] = false; 
+		}
+	}
+}
+
+void getSafeZone(int height)
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] - height <= 0) {
+				safezone[i][j] = false; 
+			}
+		}
+	}
+}
+
+void dfs(int x, int y)
+{
+	if (x >= 0 && x < n && y >= 0 && y < n)
+	{
+		if (!visited[x][y] && safezone[x][y]) {
+			visited[x][y] = true; 
+
+			dfs(x - 1, y);
+			dfs(x + 1, y);
+			dfs(x, y - 1);
+			dfs(x, y + 1);
+		}		
+	}
+
+}
+
+int main()
+{
+	int maxHeight = 0; 
+	int maxCity = 0; 
+
+	cin >> n; 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j]; 
+			maxHeight = max(maxHeight, board[i][j]);
+		}
+	}
+	
+	for (int h = 0; h < maxHeight; h++) {
+
+		initSafeZoen(); 
+		getSafeZone(h); 
+		
+		int sum = 0; 
+		//
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+
+				if (!visited[i][j] && safezone[i][j]) {
+					dfs(i, j);
+					sum++; 					
+				}
+			}
+		}
+ 		maxCity = max(maxCity, sum);
+	}
+
+	cout << maxCity << endl; 
+
+}
