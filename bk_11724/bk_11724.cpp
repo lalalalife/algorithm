6bf14dc0b4fdc19fcb39098e54ca848ee03d3e7a
lalalalife <hongoops@gmail.com>
2023-05-24 06:24:39 +0900

    백준dfs

---
+++ bk_11724/bk_11724.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std; 
+
+int n, m; 
+vector<int> v[1001]; 
+bool visited[1001]; 
+
+void dfs(int index)
+{
+	for (int i = 0; i < v[index].size(); i++) {
+		int nextIndex = v[index][i]; 
+		if (!visited[nextIndex]) {
+			visited[nextIndex] = true; 
+			dfs(nextIndex); 
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> m; 
+	for (int i = 1; i <= m; i++) {
+		int a, b; 
+		cin >> a >> b; 
+		v[a].push_back(b); 
+		v[b].push_back(a); 
+	}
+
+	int cnt = 0; 
+	for (int i = 1; i <= n; i++)
+	{
+		if (!visited[i]) {
+			dfs(i); 
+			cnt++; 
+		}
+	}
+
+	cout << cnt << endl; 
+}
