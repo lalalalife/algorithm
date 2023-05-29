424a6eab43982c752445960912a2c359787e407e
lalalalife <hongoops@gmail.com>
2023-05-30 06:31:02 +0900

    백준 그리디

---
+++ bk_11047/bk_11047.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std; 
+
+int n, k; 
+int type[10];
+
+int main()
+{
+	cin >> n >> k; 
+	for (int i = 0; i < n; i++) {
+		cin >> type[i]; 
+	}
+
+	int cnt = 0; 
+	int num = k; 
+
+	for (int i = n-1; i >= 0; i--) {
+		if (type[i] > num)
+			continue; 
+
+		int mok = num / type[i];
+		cnt += mok; 
+
+		num = num % type[i]; 
+	}
+
+	cout << cnt << endl; 
+}
+
