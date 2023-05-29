1b88b93fbf8c3ec6dbc334417c66a93f1068d9c0
lalalalife <hongoops@gmail.com>
2023-05-30 06:14:58 +0900

    백준 그리디

---
+++ bk_2810/bk_2810.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std; 
+
+int n; 
+char board[50]; 
+
+int main()
+{
+    int coupleCount = 0; 
+    int cupCount = 0; 
+
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        cin >> board[i]; 
+
+        if (board[i] == 'L') {
+            coupleCount++; 
+
+            if (coupleCount % 2 == 0)
+                cupCount++;
+        }
+        else if (board[i] == 'S') {
+            cupCount++; 
+        }
+    }
+    cupCount++; 
+    if (cupCount > n)
+        cout << n << endl; 
+    else
+        cout << cupCount << endl; 
+}
