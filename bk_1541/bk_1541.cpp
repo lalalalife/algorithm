dd242d837841878a329b09d2b7aabd645a2ae65f
lalalalife <hongoops@gmail.com>
2023-06-02 06:35:41 +0900

    백준 그리디

---
+++ bk_1541/bk_1541.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std; 
+
+char arr[51]; 
+
+int main()
+{
+	cin >> arr; 
+	int len = strlen(arr); 
+
+	int isMinus = false; 
+	string num; 
+	int result = 0; 
+
+	for (int i = 0; i <= len; i++) {
+		
+		if (arr[i] == '+' || arr[i] == '-' || i == len) {
+
+			if (isMinus) {
+				result -= atoi(num.c_str());
+			}
+			else {
+				result += atoi(num.c_str());
+			}
+
+			if (arr[i] == '-')
+				isMinus = true;
+
+			num = ""; 			
+		}
+		else {
+			num += arr[i]; 
+		}
+	}
+
+	cout << result << '\n'; 
+}
