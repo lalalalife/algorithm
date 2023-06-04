97afdd1115f7d607a33b52b7ef3bc33d92f24cf5
lalalalife <hongoops@gmail.com>
2023-06-04 11:05:35 +0900

    백준 그리디

---
+++ bk_16953/bk_16953.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std; 
+
+long a, b; 
+
+int main()
+{
+	cin >> a >> b; 
+
+	int cnt = 0; 
+	while (true) {
+		
+		if (a == b) {
+			cnt++;
+			break; 
+		}
+		if (a > b) {
+			cnt = -1; 
+			break; 
+		}
+			
+
+		if (b % 2 == 0) {
+			b /= 2;			
+		}
+		else if (b % 10 == 1) {
+			b--; 
+			b /= 10; 
+		}
+		else {
+			cnt = -1; 
+			break; 
+		}
+
+		cnt++; 
+	}
+	
+	cout << cnt << endl; 
+}
