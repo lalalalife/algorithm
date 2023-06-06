24b686aa963500117125055a23cd0f0383373603
lalalalife <hongoops@gmail.com>
2023-06-07 06:57:54 +0900

    백준 그리디

---
+++ bk_2217/bk_2217.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+int n; 
+int arr[100000]; 
+
+int main()
+{
+	cin >> n; 
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i]; 
+	}
+
+	sort(arr, arr + n); 
+
+	int maxW = 0; 
+
+	for (int i = 0; i < n; i++) {		
+		maxW = max(maxW, arr[i] * (n - i)); 
+	}
+	cout << maxW << endl; 
+}
+
