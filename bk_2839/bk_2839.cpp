009924108dae9743f35e5157f5a00f17519a6f8b
lalalalife <hongoops@gmail.com>
2023-06-16 06:42:19 +0900

    백준 그리디

---
+++ bk_2839/bk_2839.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std; 
+
+int n; 
+
+int main()
+{
+    cin >> n; 
+
+    int result = 0; 
+
+    while (n > 0) {
+
+        if (n % 5 == 0) {            
+            result += n / 5; 
+            n = 0;
+            break;             
+        }
+
+        n -= 3; 
+        result++; 
+    }
+
+    if (n != 0)
+        result = -1; 
+
+    cout << result << endl;
+    
+}
