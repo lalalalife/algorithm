8eee2607b14cfe71bc07b07525622ef7bcf8373f
lalalalife <hongoops@gmail.com>
2023-06-17 08:37:46 +0900

    백준 그리디

---
+++ bk_1931/bk_1931.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std; 
+
+int n; 
+vector<pair<int, int>> v; 
+
+bool comp(pair<int, int> a, pair<int, int> b) {
+    if (a.second == b.second)
+        return a.first < b.first; 
+
+    return a.second < b.second; 
+}
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        int a, b; 
+        cin >> a >> b; 
+
+        v.push_back({ a, b }); 
+    }
+
+    sort(v.begin(), v.end(), comp); 
+
+    int endTime = v[0].second; 
+    int count = 1; 
+
+    for (int i = 1; i < n; i++) {
+        if (v[i].first >= endTime) {
+            count++; 
+            endTime = v[i].second; 
+        }
+    }
+
+    cout << count << endl; 
+}
