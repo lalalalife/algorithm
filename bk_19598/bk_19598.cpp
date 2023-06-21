108ecc63da0e3061a0a389afcde7491a84d6b50a
lalalalife <hongoops@gmail.com>
2023-06-22 06:17:49 +0900

    백준 그리디

---
+++ bk_19598/bk_19598.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std; 
+#include <vector>
+#include <algorithm>
+
+int n; 
+vector<pair<int, int>> v; 
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        int a, b;         
+        cin >> a >> b; 
+        v.push_back({ a, 1 }); 
+        v.push_back({ b, 1 });
+    }
+    sort(v.begin(), v.end()); 
+    
+    int cnt = 0; 
+    int result = 0; 
+    for (int i = 0; i < v.size(); i++) {
+
+        cnt += v[i].second; 
+        result = max(result, cnt);
+    }
+
+    cout << result << endl; 
+}
