0d7297e81fde0d0c1a42d88e09934fe135d09c98
lalalalife <hongoops@gmail.com>
2023-06-26 06:24:17 +0900

    백준 그리디

---
+++ bk_14916/bk_14916.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std; 
+
+int n; 
+int main()
+{
+    cin >> n; 
+
+    int cnt = 0; 
+    
+    if (n % 5 == 0) {
+        cout << n / 5 << endl; 
+    }
+    else {
+        while (n > 0) {
+            n -= 2; 
+            cnt++; 
+            if (n % 5 == 0) {    
+                cnt += n / 5;                 
+                cout << cnt << endl;                
+                break; 
+            }
+        }
+    }
+
+    if (n < 0)
+        cout << "-1" << endl;
+    
+    return 0; 
+}
