572708f3c3adfa62e6dae5afd5be080c2b68e370
lalalalife <hongoops@gmail.com>
2023-06-29 06:24:54 +0900

    백준그리디

---
+++ bk_18310/bk_18310.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std; 
+#include <vector>
+#include <algorithm>
+
+int n; 
+vector<int> v; 
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        int x; 
+        cin >> x; 
+        v.push_back(x); 
+    }
+    sort(v.begin(), v.end()); 
+
+    int mid = n / 2; 
+    int remain = n % 2; 
+
+    int result = v[mid + remain - 1]; 
+    cout << result << endl;
+}
