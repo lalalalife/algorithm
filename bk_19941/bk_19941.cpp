c6190985b14ed900c7d21ff4cf2e5e00c2d9b5fb
lalalalife <hongoops@gmail.com>
2023-07-01 09:31:14 +0900

    백준 그리디

---
+++ bk_19941/bk_19941.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+using namespace std; 
+
+char arr[200001]; 
+int n, k; 
+
+int main()
+{
+    cin >> n >> k; 
+    getchar();
+
+    memset(arr, 'X', sizeof(arr)); 
+
+    for (int i = 1; i <= n; i++) {
+        scanf_s("%c", &arr[i]);         
+    }
+
+    int cnt = 0; 
+
+    for (int i = 1; i <= n; i++) {
+
+        if (arr[i] == 'P') {
+
+            int s = max(0, i - k); 
+            int e = min(200001, i + k);
+
+            for (int j = s; j <= e; j++) {
+                if (arr[j] == 'H') {
+                    arr[j] = 'X'; 
+                    cnt++; 
+                    break; 
+                }
+            }
+        }
+    }
+
+    cout << cnt << endl; 
+}
