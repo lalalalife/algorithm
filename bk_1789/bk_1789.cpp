47b5bff2e526cb5791319df292da78db01e050e2
lalalalife <hongoops@gmail.com>
2023-07-05 06:04:43 +0900

    백준 그리디

---
+++ bk_1789/bk_1789.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std; 
+
+long long s;
+
+int main()
+{
+    cin >> s; 
+    
+    long long sum = 0; 
+    long long num = 0;
+
+    while (true) {
+        sum += ++num; 
+
+        if (sum > s) {
+            cout << --num << endl; 
+            return 0; 
+        }
+        else if (sum == s) {
+            cout << num << endl; 
+            return 0; 
+        }
+    }
+}
