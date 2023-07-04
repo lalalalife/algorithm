90cd18f149f10bc098b91635ccb1c2ae5394b6c4
lalalalife <hongoops@gmail.com>
2023-07-05 06:29:13 +0900

    백준 그리디

---
+++ bk_11399_2/bk_11399_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std; 
+
+int n;
+int arr[1000]; 
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {        
+        cin >> arr[i];         
+    }
+
+    sort(arr, arr+n); 
+
+    int tmp = 0; 
+    int sum = 0; 
+
+    for (int i = 0; i < n; i++) {
+        tmp += arr[i]; 
+        sum += tmp; 
+    }
+
+    cout << sum << endl; 
+
+    return 0; 
+}
