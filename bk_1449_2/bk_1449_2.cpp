24395d1099fc3d1c75d7d1fa9b3cc016c97a2311
lalalalife <hongoops@gmail.com>
2023-07-06 06:13:50 +0900

    백준 그리디

---
+++ bk_1449_2/bk_1449_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+int n, l; 
+int arr[1001]; 
+
+int main()
+{
+	cin >> n >> l; 
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i]; 
+	}
+
+	sort(arr, arr + n); 
+
+	int s = 0; 
+	int e = 0; 
+	int cnt = 0; 
+
+	for (int i = 0; i < n; i++) {
+
+		if (e >= arr[i]) {
+			continue; 
+		}
+
+		s = arr[i] - 0.5; 
+		e = s + l;
+		cnt++; 
+	}
+
+	cout << cnt << endl; 
+}
+
