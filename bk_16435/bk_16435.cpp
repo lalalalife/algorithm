a41b74ac4e5bb202db8503d3372cf9b1ee823feb
lalalalife <hongoops@gmail.com>
2023-07-07 06:13:09 +0900

    백준 그리디

---
+++ bk_16435/bk_16435.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+int n, l; 
+int arr[1000]; 
+
+int main()
+{
+	cin >> n >> l; 
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i]; 
+	}
+
+	sort(arr, arr + n); 
+	
+	for (int i = 0; i < n; i++) {
+		if (l >= arr[i]) {
+			l++; 
+		}
+	}
+
+	cout << l << endl; 
+	return 0; 
+}
