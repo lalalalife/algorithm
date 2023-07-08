44262c76bf3064ea989d46a994e1cb481eab51d0
lalalalife <hongoops@gmail.com>
2023-07-08 10:27:44 +0900

    백준 그리디

---
+++ bk_14469/bk_14469.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std; 
+
+int n; 
+vector<pair<int, int>> v; 
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        int a, b; 
+        cin >> a >> b; 
+        v.push_back({ a, b }); 
+    }
+
+    sort(v.begin(), v.end()); 
+
+    int result = 0; 
+
+    for (int i = 0; i < v.size(); i++) {
+        int start = v[i].first; 
+        int cost = v[i].second; 
+
+        if (start >= result) {
+            result = start + cost; 
+        }
+        else {
+            result = result + cost; 
+        }
+    }
+
+    cout << result << '\n'; 
+
+}
