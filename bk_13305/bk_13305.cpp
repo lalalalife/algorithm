f0404f357e3ea1ed026a3e5b9003b7d284ff272d
lalalalife <hongoops@gmail.com>
2023-07-09 06:28:10 +0900

    백준 그리디

---
+++ bk_13305/bk_13305.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std; 
+
+int n; 
+int dist[100000]; 
+int cost[100000];
+
+int main()
+{    
+    cin >> n; 
+    for (int i = 1; i < n; i++) {
+        cin >> dist[i]; 
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> cost[i];
+    }
+
+    long long totalCost = 0;
+
+    int nowCost = cost[0]; 
+    totalCost = nowCost * dist[1];
+    
+    for (int i = 1; i < n; i++) {
+        if (nowCost > cost[i]) {
+            nowCost = cost[i];
+        }
+                    
+        totalCost += nowCost * dist[i + 1];        
+    }
+
+    cout << totalCost << endl; 
+}
