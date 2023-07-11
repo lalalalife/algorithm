f8322f0f6340c852330cc49b28f4196489320a1d
lalalalife <hongoops@gmail.com>
2023-07-11 10:08:37 +0900

    백준 그리디

---
+++ bk_2828/bk_2828.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std; 
+
+int n, m, c; 
+int arr[11]; 
+
+int main()
+{
+    cin >> n >> m; 
+    cin >> c; 
+    for (int i = 1; i <= c; i++) {
+        cin >> arr[i]; 
+    }
+
+    int basketStart = 1; 
+    int basketEnd = basketStart + (m-1); 
+    int result = 0; 
+
+    for (int i = 1; i <= c; i++) {
+        if (basketStart <= arr[i] && arr[i] <= basketEnd)
+            continue; 
+
+        if (arr[i] > basketEnd) {
+            int gap = arr[i] - basketEnd; 
+            result += gap; 
+            basketEnd += gap; 
+            basketStart += gap; 
+        }
+        else if (arr[i] < basketStart) {
+            int gap = basketStart - arr[i]; 
+            result += gap; 
+            basketEnd -= gap;
+            basketStart -= gap;
+        }
+    }
+
+    cout << result << endl; 
+}
