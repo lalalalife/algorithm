4376b367a4bb8bf703c82ac5e9e546fba89303e1
lalalalife <hongoops@gmail.com>
2023-07-13 06:04:52 +0900

    백준 그리디

---
+++ bk_1758/bk_1758.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std; 
+
+int n; 
+int arr[100000]; 
+
+bool comp(int a, int b) {
+    return a > b; 
+}
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; 
+    }
+
+    sort(arr, arr + n, comp); 
+
+    long long result = 0; 
+    for (int i = 0; i < n; i++) {
+        int tip = arr[i] - i; 
+        if(tip > 0)
+            result += tip; 
+    }
+
+    cout << result << endl; 
+}
