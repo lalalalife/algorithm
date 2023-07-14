0e28d973c4649453d855753006513ecd1fa4be90
lalalalife <hongoops@gmail.com>
2023-07-15 07:48:48 +0900

    백준 그리디

---
+++ bk_2847/bk_2847.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std; 
+
+int n; 
+int arr[100]; 
+
+int main()
+{
+    cin >> n; 
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; 
+    }
+
+    int result = 0; 
+
+    for (int i = n-1; i > 0; i--) {
+        if (arr[i] <= arr[i - 1]) {
+            int tmp = arr[i - 1]; 
+            arr[i - 1] = arr[i] - 1; 
+            result += tmp - arr[i - 1]; 
+        }
+    }
+
+    cout << result << endl; 
+}
+
